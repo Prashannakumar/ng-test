099a273a2f85ac997b07aaf5e3b2c0305178133d
Prashannakumar <a.prashanna1996@gmail.com>
2024-09-15 08:20:28 +0530

    multiple image upload

--- src/app/custom-upload/custom-upload.component.ts
+++ src/app/custom-upload/custom-upload.component.ts
@@ -1,13 +1,13 @@
 import { Component, signal, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-upload',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatIconModule, CommonModule ],
+  imports: [FormsModule, ReactiveFormsModule, MatIconModule, CommonModule],
   templateUrl: './custom-upload.component.html',
   styleUrl: './custom-upload.component.less'
 })
@@ -21,23 +21,36 @@ export class CustomUploadComponent {
   uploadSuccess: boolean = false;
   uploadError: boolean = false;
 
-  constructor(private snackBar: MatSnackBar) {}
+  selectedImageFiles: { file: File, imageName: string, fileSize: number, imagePreview: string }[] = [];
+
+  constructor(private snackBar: MatSnackBar) { }
 
   // Handler for file input change
-  onFileChange(event: any): void {
-    const file = event.target.files[0] as File | null;
-    this.uploadFile(file);
+  onFileChange(event: any, multiple?: 'multiple'): void {
+    if (multiple) {
+      const files = event.target.files as FileList | null;
+      this.uploadMultipleFile(files);
+    } else {
+      const file = event.target.files[0] as File | null;
+      this.uploadFile(file);
+    }
   }
 
   // Handler for file drop
-  onFileDrop(event: DragEvent): void {
-    event.preventDefault();
-    const file = event.dataTransfer?.files[0] as File | null;
-    this.uploadFile(file);
+  onFileDrop(event: DragEvent, multiple?: 'multiple'): void {
+    if (multiple) {
+      event.preventDefault();
+      const files = event.dataTransfer?.files as FileList | null;
+      this.uploadMultipleFile(files);
+    } else {
+      event.preventDefault();
+      const file = event.dataTransfer?.files[0] as File | null;
+      this.uploadFile(file);
+    }
   }
 
   // Prevent default dragover behavior
-  onDragOver(event: DragEvent): void {
+  onDragOver(event: DragEvent, multiple?: 'multiple'): void {
     event.preventDefault();
   }
 
@@ -66,14 +79,46 @@ export class CustomUploadComponent {
     }
   }
 
+  // Method to handle multiple file upload
+  uploadMultipleFile(files: FileList | null): void {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (file && file.type.startsWith('image/')) {
+        const fileSize = Math.round(file.size / 1024);
+        const imageName = file.name;
+        const reader = new FileReader();
+        var imagePreview: string = null;
+        reader.onload = (e) => {
+          imagePreview = e.target?.result as string; // Set image preview URL
+          this.selectedImageFiles.push({ file: file, fileSize: fileSize, imageName: imageName, imagePreview: imagePreview });
+        };
+        reader.readAsDataURL(file);
+        // this.selectedImageFiles.push({ file: file, fileSize: fileSize, imageName: imageName, imagePreview: imagePreview });
+        this.uploadSuccess = true;
+        this.uploadError = false;
+      } else {
+        this.uploadSuccess = false;
+        this.uploadError = true;
+        this.snackBar.open('Only image files are supported!', 'Close', {
+          duration: 3000,
+          panelClass: 'error',
+        });
+      }
+    }
+  }
+
   // Method to remove the uploaded image
-  removeImage(): void {
-    this.selectedFile = null;
-    this.imageName.set('');
-    this.fileSize.set(0);
-    this.imagePreview.set('');
-    this.uploadSuccess = false;
-    this.uploadError = false;
-    this.uploadProgress.set(0);
+  removeImage(multiple?: {index: number, data: any}): void {
+    if (multiple) {
+      console.log(multiple)
+    } else {
+      this.selectedFile = null;
+      this.imageName.set('');
+      this.fileSize.set(0);
+      this.imagePreview.set('');
+      this.uploadSuccess = false;
+      this.uploadError = false;
+      this.uploadProgress.set(0);
+    }
   }
 }
